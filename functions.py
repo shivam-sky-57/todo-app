0b3e84db75bb28404e11d06aede7ce4ed3483a4d
shivam-sky-57 <shivamkumar7965@gmail.com>
2025-05-10 00:47:14 +0530

    Implement the Add button

--- functions.py
+++ functions.py
@@ -13,7 +13,7 @@ def write_todos(todos_arg,filepath=FILEPATH):
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
 
-print(__name__)
+
 if __name__ == "__main__":
     print("Hello")
     print(get_todos())
